a86934b17d82d5f98e34ed411b4f9a04710e84c9
Ettore Bevilacqua <ettorebevilacqua@gmail.com>
2022-03-07 14:40:42 +0100

    spiegazione canvas e fillRect

--- a.js
+++ a.js
@@ -18,7 +18,20 @@ function cubo(x, y){
 
 function draw(){
     sfondo();
-    cubo(20, 10);
+    //cubo(20, 20);
 }
 
 draw();
+
+
+ctx.fillStyle = 'white';
+// rettangolo a distanza dal bordo sinistro di 10px, e superiore di 20px, 
+// il rettangolo deve essere lungo 40px e alto 80
+
+// funzione filleRect
+// permette di disegnare un rettangolo (quandrato quando i lati sono uguali)
+// i primni due parametri è la distanza dal asse x, e la distanza dal asse y
+// l'origine del canvas è x=0, y=0 è si trova nel angolo superiore a sinistra
+
+
+ctx.fillRect(10, 40, 40, 40);
