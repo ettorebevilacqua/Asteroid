817b2bd41cb2578f2e278ca2ff7a965419aa736d
Ettore Bevilacqua <ettorebevilacqua@gmail.com>
2022-03-07 17:36:50 +0100

    spiegazione canvas e fillRect

--- README.md
+++ README.md
@@ -1,3 +1,25 @@
-Primi appunti da prendere 
 
-aaaa
+parole nuove :
+
+canvas / contesto 2d del canvas
+
+interfaccia : interfaccia utente  , interfaccia a caratterri o grafica
+
+terminale : interfaccia a caratteri
+
+funzione : fa qualcosa quando la chiamo con le parentesi
+
+metodo : una funzione dentro a un oggetto (quando scrivo qualcosa. )
+Api : application program interface : interfaccia a una applicazione, sono le funzioni che mette a disposizione una applicazia o un libreria, in genere ci si riferisce a una libreria.
+
+il context del canvas ci da le api quindi le funzioni per poter disegnare sul canvas, ctx è il nome della varibile da me scelto per poter chiamare le funzioni di disegno.
+
+il canvas è la lavagna di disegno , il context sono le funzioni per poter disegnare
+
+git : branch (lavorre e spostarsi da branch)
+
+
+
+
+
+
--- a.js
+++ a.js
@@ -1,7 +1,34 @@
 const canvas = document.getElementById('canvas');
 const ctx = canvas.getContext('2d');
 
-ctx.fillRect(10, 40, 40, 40);
+// guida https://www.html.it/pag/19303/canvas1/
+
+var immagine = new Image();
+immagine.src = "https://www.rotomail.it/wp-content/uploads/revslider/slidero-ok2/SFONDO-VIDEO.jpg";
+// ctx.drawImage(immagine,0,0);
+
+ctx.beginPath(); // voglio una fare una serie di linee.
+// sposta il cursore del path nella posizione 40,170
+ctx.moveTo(0,0);
+// crea un subpath linea fino alla posizione 260,170
+  ctx.lineTo(100,0);
+  ctx.lineTo(200,90);
+  ctx.lineTo(100,90);
+  // ctx.closePath();
+ //ctx.lineTo(0,50);
+// crea un subpath arco che sia tangente alle due linee (260,170 - 150,250)
+// (150,250 - 40,170) con diametro 150
+//ctx.lineTo(40,170);
+//ctx.moveTo(150,170);
+//ctx.lineTo(150,40);
+//ctx.rect(150,40,60,30);
+// disegna tutti i subpath del path corrente sul canvas usando la
+// configurazione del drawing state
+  ctx.stroke();
+// riempi tutti le aree inscritte dal path corrente usanto la configurazione
+// del deawing state
+ctx.fill();
+
 
 // disegnare rettangolo a distanza dal bordo sinistro di 10px, e superiore di 20px, 
 // il rettangolo deve essere lungo 40px e alto 80
