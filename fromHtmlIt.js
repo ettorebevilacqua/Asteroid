3b79bcfde0b59dff951f1aa257a87b312c34a886
Ettore Bevilacqua <ettorebevilacqua@gmail.com>
2022-03-10 02:25:40 +0100

    promemoria

---
+++ fromHtmlIt.js
@@ -0,0 +1,49 @@
+
+// fsdkfsdk sdfs sd sdf 
+// var 
+
+"use strict";
+
+// varconst pippo = 22 // var pippo=22;
+
+const pippo=22; // inutoile 
+7== pippo='ssss';
+
+let = a; //
+
+!piove
+
+
+((lui ama lei) AND (lei ama lui)) 
+AND 
+(lui ha i soldi ) OR (lei ha i soldi )
+(i genitori di lui sono daccordo ) AND (i genitori di lei sono daccordo)
+and 
+(lui è fertile ) and (lei è fertile)
+(lui è fertile ) and (lei è fertile)
+(lui è fertile ) and (lei è fertile)
+
+
+2a+ 3b - 4b + 5a
+7a -b
+00101 << 2 = 10100 00// a riv edere se hanno capito 
+const espressione=(leiAma && LuiAma) && (luiSoldi || leiSoldi) ;
+const espressione=(true && true) && ( true || false) ;
+const espressione=(true) && (true) ;
+const espressione=true ;
+
+const ris= espressione ? 'si sposano' : 'non si sposano'
+
+1001001001 >> 0100100100 
+false && false = false;
+false && true = false
+true && false = false;
+true && true = true;
+
+ (false nor false) = true;
+ (false nor true) = falsa;
+ (true nor false) = false;
+ (true nor true) = false;
+
+not false = true
+not true = false
